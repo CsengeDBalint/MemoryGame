7e6b106a644e888797217d2a6d7231a1b8db5cbc
Csenge <34919849+CsengeDBalint@users.noreply.github.com>
2018-05-13 15:46:11 +0200

    Updated commits,

--- js/app.js
+++ js/app.js
@@ -1,70 +1,77 @@
 //An array that holds all of the cards
 const cardsArray = [
-    "fa-diamond", 
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
+    "fa-power-off", 
+    "fa-plug",
+    "fa-print",
+    "fa-save",
+    "fa-keyboard-o",
     "fa-headphones",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
+    "fa-desktop",
+    "fa-database",
+    "fa-power-off", 
+    "fa-plug",
+    "fa-print",
+    "fa-save",
+    "fa-keyboard-o",
     "fa-headphones",
-    "fa-leaf",
-    "fa-bicycle"
+    "fa-desktop",
+    "fa-database",
 ];
 
-//DISPLAY THE CARDS ON THE PAGE
+//Basic variables
 
 let openedCardsArray = []; // Temporary array for viewed cards
 let matchedCardsArray = []; // Temporary array for matched cards
 let shuffledCardsArray = []; // Temporary array for all mixed cards
-let ratingStarsNumber;
-let moveCounter = 0;
 
 
 const deck = document.querySelector("ul.deck");
-const stars = document.querySelectorAll(".fa-star");
-// Start a game: a function calling shuffle() and createDeck() functions
+
+
+// START A GAME: a function calling shuffle() and createDeck() functions
 function startGame() {
     shuffledCardsArray = shuffle(cardsArray);
+    
     clearDeck();
-    createDeck(shuffledCardsArray);
-    stars[2].classList.add("purple");
-    stars[1].classList.add("purple");
+    
+    createDeck(shuffledCardsArray); //Add new "icons" to every card by calling the createDeck() function 
+    
+    stars[2].classList.add("purple"); //Set back the second rating star 
+    
+    stars[1].classList.add("purple"); //Set back the third rating star 
 
 }
 
-//Loop through every item of the shuffledCardsArray
+//Function to loop through every item of the shuffledCardsArray with an icon
 function createDeck(shuffledCardsArray) {
 
     for (var i = 0; i < shuffledCardsArray.length; i++) {
         
-        let liEl = document.createElement('li');
+        let liEl = document.createElement("li"); //Create element <li>
+        
+        liEl.classList.add("card"); // Add .card to <li>
+        
+        let iEl = document.createElement("i"); // Create element <i>
+        
+        iEl.classList.add("fa"); // Add .fa to <i>
+        
+        iEl.classList.add(shuffledCardsArray[i]);  // Also add .fa-x to <i> 
+		
+        liEl.appendChild(iEl); // Append the icon <i> to the <li> element 
+        
+        deck.appendChild(liEl); // Append every <li> element to the deck
 
-        liEl.classList.add('card');
-
-        let iEl = document.createElement('i');
-
-        iEl.classList.add('fa');
-        iEl.classList.add(shuffledCardsArray[i]);
-
-        liEl.appendChild(iEl);
-        deck.appendChild(liEl);
-
-        liEl.addEventListener('click', turnCard);
+        liEl.addEventListener("click", turnCard); // Add eventListener to the created card element
     }
-
 }
 
+//Function to remove every card from deck
 function clearDeck() {
     
-    moves.innerHTML = 0;
-    let currDeck = document.querySelectorAll('li.card');
+    moves.innerHTML = 0; //Set moveCounter to 0
+    
+    let currDeck = document.querySelectorAll("li.card");
+    
     for(let i = 0; i < currDeck.length ; i++) {
 		currDeck[i].remove();
     }
@@ -76,66 +83,78 @@ function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
+        
         randomIndex = Math.floor(Math.random() * currentIndex);
+        
         currentIndex -= 1;
+        
         temporaryValue = array[currentIndex];
+        
         array[currentIndex] = array[randomIndex];
+        
         array[randomIndex] = temporaryValue;
     }
 
     return array;
-
 }
-// end of provided shuffle function
 
-// PLAY GAME
+
+// PLAY GAME: On every clicked card will be called this function 
 function turnCard(event) {
 
-    event.target.classList.add('open', 'show'); //If turned up add classes "open" and "show" to liEl
+    event.target.classList.add("open", "show"); //If turned up add classes "open" and "show" to liEl
+    
+    event.target.removeEventListener("click", turnCard); // Disable clicking and turning on the same card twice
 
-    event.target.removeEventListener('click', turnCard); // Disable clicking and turning on the same card twice
-
-    checkCards(event); //Run the checkCards() for every clicked card
-
+    checkCards(event); 
 }
 
+//Function to compare the cards if there are two of them opened 
 function checkCards(event) {
 
-    openedCardsArray.push(event.target.firstChild);
+    openedCardsArray.push(event.target.firstChild); 
 
-    if (openedCardsArray.length === 2) {
-          
-         moveCount();
-         updateRatingStars();
+    if (openedCardsArray.length === 2) { 
+        
+        deck.style.pointerEvents = "none"; // After turning two cards there is no possible turning other cards for 1 second 
+        
+        moveCount(); // With every two cards, moveCounter increments by 1
+        
+        updateRatingStars(); // By every move the rating stars number will be updated
+        
       
         if(openedCardsArray[0].className === openedCardsArray[1].className) {
 
-            openedCardsArray[0].parentNode.classList.add('match');
-            openedCardsArray[1].parentNode.classList.add('match');
+            openedCardsArray[0].parentNode.classList.add("match");
+            openedCardsArray[1].parentNode.classList.add("match");
 
             matchedCardsArray.push(openedCardsArray[0]);
             matchedCardsArray.push(openedCardsArray[1]);
 
             openedCardsArray = []; //Empty openedCardsArray
+            deck.style.pointerEvents = "auto";
 
         } else {
 
-            setTimeout(function() {
+            setTimeout(function() { //Function that both cards of the array makes clickable  after a second again
                 for (let i = 0; i < openedCardsArray.length; i++) {
-                    openedCardsArray[i].parentNode.addEventListener('click', turnCard);
+                    openedCardsArray[i].parentNode.addEventListener("click", turnCard);
                 }
             }, 1000);
 
             setTimeout(turnBackCard, 1000);
 
         }
-
+    
     }
 
-    // END GAME if matchedCardsArray holds 16 cards
-    if (matchedCardsArray.length === 2) {
+
+    // END GAME: if there are 8 pairs in the matchedCardsArray open the Modal window and make it clickable
+    if (matchedCardsArray.length === 16) {
     
-    openModal();
+        openModal();
+        document.body.style.pointerEvents = "auto";
+        deck.style.pointerEvents = "none"; // Prevent all cards of the deck from being clickable
     
     }
 
@@ -145,43 +164,51 @@ function checkCards(event) {
 function turnBackCard() {
 
     for (let i = 0; i < openedCardsArray.length; i++) {
-        openedCardsArray[i].parentNode.classList.remove('open', 'show'); //Turn back cards
+        openedCardsArray[i].parentNode.classList.remove("open", "show"); //Turn back cards
     }
-
-    openedCardsArray = [];
-
+    
+    openedCardsArray = []; //Empty openCardsArray
+    
+    deck.style.pointerEvents = "auto"; //Make the deck clickable again
 }
 
 //Add moveCounter and handle stars rating
-let moves = document.querySelector('.moves');
+let moveCounter = 0;
+let moves = document.querySelector(".moves");
 
-
-function moveCount () {
+function moveCount () { //By every turn of a card the number of moves will increased by 1
+  
   moveCounter = moveCounter+1;
-  moves.innerHTML = moveCounter;
+  
+  moves.innerHTML = moveCounter; //Hold the stand of the move counter
   
 }
 
 //  Update rating stars
-function updateRatingStars() {
+let ratingStarsNumber;
+
+const stars = document.querySelectorAll(".fa-star");
+
+function updateRatingStars() { //Update gained stars number according the number of moves
     ratingStarsNumber = 3;
 
-    if (moveCounter === 1) {
-        myTimer(); 
+    if (moveCounter === 1) { 
+        myTimer(); //Start timer as the first pair of cards are turned
     }
-    else if (moveCounter === 2 || moveCounter === 3) {
-        ratingStarsNumber =3;   
-        resultStars.innerHTML = ratingStarsNumber;
-    }
-    else if (moveCounter >= 4 && moveCounter <= 7) {
-		// Remove first purple star
-        stars[2].classList.remove("purple");
+    else if (moveCounter >= 2 && moveCounter <= 20) { 
+        
+        ratingStarsNumber =3;   //Between 2 and 20 moves the user gaines 3 stars
+        resultStars.innerHTML = ratingStarsNumber;  //Save the current rating stars number
+    } 
+    else if (moveCounter >= 21 && moveCounter <= 36) {  //Between 21 and 36 moves the user gaines 2 stars
+		
+        stars[2].classList.remove("purple"); //Remove the first purple star
         ratingStarsNumber =2; 
         resultStars.innerHTML = ratingStarsNumber;
         
-	} else if (moveCounter >= 8) {
-		//Remove second purple star
-		stars[1].classList.remove("purple");
+	} else if (moveCounter >= 37) { //Above 37 moves the user gains only one star
+		
+		stars[1].classList.remove("purple"); //Remove the second purple star
         ratingStarsNumber = 1;
         resultStars.innerHTML = ratingStarsNumber; 
 	}
@@ -199,24 +226,32 @@ const resultTime = document.querySelector(".resultTime");
 
 
 
-function openModal() {
+function openModal() { //Open modal window showing the number of moves, gained rating stars number and time
 	setTimeout(function() {
+        
         modal.style.display = "block";
-        //score.textContent = `It took ${moveCounter} moves, ${ratingStarsNumber} stars and ${timer.innerHTML} time `;
+        
         resultMoves.innerHTML = moveCounter;
+        
         resultStars.innerHTML = ratingStarsNumber;
+        
         resultTime.innerHTML = timer.innerHTML; 
-         }, 1000);
-         stopTimer();	 
+    
+    }, 1000);
+        
+    stopTimer(); 
 }
+
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
+
 // Close modal
 span.onclick = function() {
     modal.style.display = "none";
 }
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+document.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
@@ -228,53 +263,64 @@ let minutes = 0;
 let seconds = 0;
 let time;
 
-//let minutes = document.querySelector(".min");
-//seconds = document.querySelector(".sec");
-
-
-function myTimer() {
+function myTimer() { //Timer counting seconds and minutes
     
     time = setInterval(function() {
-    timer.textContent = ` ${minutes} min.${seconds} sec.`;
-        seconds++;
-      
-      if (seconds === 60) {
-        minutes++;
-        seconds = 0;
-      }
+    
+        timer.textContent = ` ${minutes} min.${seconds} sec.`; //Add timer the current time
+            seconds++;
+        
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+
     }, 1000);
     
-  }
+}
 
 //Stop Timer
 function stopTimer() {
-    clearInterval(time);
+    clearInterval(time); //Stop timer as game finished
 }
 
 
-//Restart the game with the Play again! button 
+//Restart the game by clicking the Play again! button of the modal windoww
 const playAgain = document.querySelector(".playAgain");
 
-playAgain.addEventListener('click', function () {
-	modal.style.display = "none";
-    matchedCardsArray = [];
-    minutes = 0;
-    seconds = 0;
-    timer.textContent = "0min.0sec.";
-    moveCounter = 0;
+playAgain.addEventListener("click", function () {
     
-	startGame();
+    modal.style.display = "none"; // Not show modal anymore
+    
+    matchedCardsArray = []; //Empty the 8 pairs of cards from matchedCardsArray
+    
+    clearInterval(time); //Set timer to 0
+    
+    timer.textContent = "0min.0sec."; //Set timer display to 0min.0sec. 
+    
+    moveCounter = 0; //Set the move counter to 0
+    
+    deck.style.pointerEvents = "auto"; //Enable to turn the cards again
+    
+    startGame(); //Start a new game
 });
 
-//Restart the game with the Restart icon in the Panel-score
+//Restart the game by clicking the Restart icon in the Panel-score
 const restart = document.querySelector(".restart");
 
-restart.addEventListener('click', function (event) {
+restart.addEventListener("click", function (event) {
+    
     matchedCardsArray = [];
+    
     timer.textContent = "0min.0sec.";
+    
+    clearInterval(time);
+    
     moveCounter = 0;
     
-	startGame();
+    deck.style.pointerEvents = "auto";
+    
+    startGame();
 });
 
 //Start a game
